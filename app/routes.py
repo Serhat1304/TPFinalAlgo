19a6a54824c54409198b8a61b7f90909e86358ff
Hamza Bella <hamza@stevegates.co>
2025-02-11 14:25:56 +0100

    ajout entrainement et analyse

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,30 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+import joblib
+from app.db import get_db_connection, save_tweet
+from app.models import entrainer_modele
 
 bp = Blueprint('api', __name__)
 
+
+@bp.route('/entrainer', methods=['GET'])
+def entrainer():
+    entrainer_modele()
+    return jsonify({"message": "entrainé"})
+
+
 @bp.route('/analyze', methods=['POST'])
 def analyze_sentiments():
-    return jsonify({"message": "Test d'endpoint"})
+    model = joblib.load('sentiment.pkl')
+
+    tweets = request.json.get('tweets', [])
+    if not tweets:
+        return jsonify({"error": "Aucun tweet fourni"}), 400
+
+    predictions = model.predict(tweets)
+
+    scores = {tweet: float(pred) for tweet, pred in zip(tweets, predictions)}
+
+    for score in scores:
+        save_tweet(score, scores[score] == 1, scores[score] == -1)
+
+    return jsonify(scores)
