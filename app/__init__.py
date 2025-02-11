88124c58e45fb06b20467011c017143b25e13f30
Hamza Bella <hamza@stevegates.co>
2025-02-11 15:28:08 +0100

    création de la table a l'init

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 
+from setup_db import create_connection, create_table
+
+
 def create_app():
     app = Flask(__name__)
 
     # Importer et enregistrer les blueprints
     from app.routes import bp
     app.register_blueprint(bp)
+    connection = create_connection()
+
+    create_table(connection)
 
     return app
