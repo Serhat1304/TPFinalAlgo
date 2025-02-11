a60ee25b827de86ae2492c329022b0ef50e903a8
djah97123 <djahnick.gene@efrei.net>
2025-02-11 14:25:32 +0100

    ETAPE 2B

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-import sys
-import os
-from flask import Flask
-from app.routes import bp
+from app import create_app
 
-# Permet à Python de retrouver vos modules internes
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-app = Flask(__name__)
-app.register_blueprint(bp)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
